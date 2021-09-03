0611c290f5d49dbe635d0da1522b37bff50e9817
xmrbky <xmr.bky@gmail.com>
2021-09-03 21:08:11 +0300

    updated

--- mqtt.py
+++ mqtt.py
@@ -1,14 +1,19 @@
-import paho.mqtt.client as mqtt 
-import time                    
-
-def message(random_client_name, c, listen_message):     
+import paho.mqtt.client as mqtt
+import time
+###########################################
+def message(random_client_name, c, listen_message):
     print(str(listen_message.payload.decode("utf-8")))
-
-c = mqtt.Client()        
-c.on_message = message  
-c.connect("broker.mqttdashboard.com")   
-c.publish("ev-ev","GEL")             
-c.subscribe("ev-ev-ev")                
-c.loop_start()                         
-time.sleep(1)                            
-c.loop_stop()                          
+###########################################
+c = mqtt.Client()
+###########################################
+c.on_message=message
+###########################################
+c.connect("broker.mqttdashboard.com")
+###########################################
+c.publish("ev-ev","GEL")
+###########################################
+c.subscribe("ev-ev-ev")
+c.loop_start() 
+time.sleep(1)
+c.loop_stop()
+###########################################
