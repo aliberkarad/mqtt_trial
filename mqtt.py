3ac78c362624a7da10b63b0b900698d5f1701316
Berkay Karadeniz <43043692+aliberkarad@users.noreply.github.com>
2021-09-03 20:43:26 +0300

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,18 +1,14 @@
-import paho.mqtt.client as mqtt #MQTT library for Python
-import time                     #Time library for listening time
+import paho.mqtt.client as mqtt 
+import time                    
 
-def message(random_client_name, c, listen_message):       #MQTT message listening function
+def message(random_client_name, c, listen_message):     
     print(str(listen_message.payload.decode("utf-8")))
 
-c = mqtt.Client()        #called into "c" MQTT Client 
-
-c.on_message = message   #message function called and started wait
-
-c.connect("broker.mqttdashboard.com")   #Connecting MQTT Broker
-
-c.publish("ev-ev","GEL")                # "GEL" message sent to broker with "ev-ev" topic
-
-c.subscribe("ev-ev-ev")                 # Client is subscribing "ev-ev-ev" topic for listen to message
-c.loop_start()                          # Listening Started...
-time.sleep(1)                           # 
-c.loop_stop()                           # Listening Stopped. 
+c = mqtt.Client()        
+c.on_message = message  
+c.connect("broker.mqttdashboard.com")   
+c.publish("ev-ev","GEL")             
+c.subscribe("ev-ev-ev")                
+c.loop_start()                         
+time.sleep(1)                            
+c.loop_stop()                          
